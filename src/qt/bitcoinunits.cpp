b17e237d73fdae7a80c0e0cd55064758c6079990
root <root@n9.yeramian.net>
2014-04-03 12:49:32 +0200

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("SBC");
+    case mBTC: return QString("mSBC");
+    case uBTC: return QString::fromUtf8("μSBC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Simbacoins");
+    case mBTC: return QString("Milli-Simbacoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Simbacoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
